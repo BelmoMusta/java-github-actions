508d0af51d3b05a3444bec1b7e76c023ece8127d
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2026-09-20 19:56:39 -0400

    init action

--- src/main/java/test/main/MainTest.java
+++ src/main/java/test/main/MainTest.java
@@ -1,10 +1,16 @@
 package test.main;
 
 import org.mustabelmo.java.githubactions.Core;
+import org.mustabelmo.java.githubactions.Properties;
+
+import java.util.Map;
 
 public class MainTest {
     public static void main(String[] args) {
         String test = Core.getInput("test");
+        for (Map.Entry<String, String> stringStringEntry : Properties.CORE_PROPERTIES.entrySet()) {
+            System.out.println("Key : "+stringStringEntry.getKey() + ", value: " + stringStringEntry.getValue());
+        }
         System.out.println("this a test for " + test);
     }
 }
