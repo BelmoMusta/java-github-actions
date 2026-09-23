e9f9608f5699acc23a0d648452820a9432033abe
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2026-09-22 21:31:55 -0400

    init action

--- src/main/java/test/main/MainTest.java
+++ src/main/java/test/main/MainTest.java
@@ -4,7 +4,7 @@ import org.mustabelmo.java.githubactions.Core;
 
 public class MainTest {
     public static void main(String[] args) {
-        String inputs = Core.getInput("__inputs");
+        String inputs = Core.getInput("test");
         System.out.println("Inputs == " + inputs);
     }
 }
