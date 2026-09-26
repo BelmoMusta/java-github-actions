c92fb01b4dde33659fb5f06fca9d50427efa57fc
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2026-09-25 21:53:55 -0400

    demo

--- src/main/java/io/github/belmomusta/demo/MonAction.java
+++ src/main/java/io/github/belmomusta/demo/MonAction.java
@@ -4,8 +4,10 @@ package io.github.belmomusta.demo;
 import io.github.belmomusta.java.actions.sdk.Action;
 import io.github.belmomusta.java.actions.sdk.Core;
 
-public class MonAction implements Action {
-    @Override
+public class MonAction
+       // implements Action
+{
+    //@Override
     public void run() throws Exception {
         Core.info("Hello World");
     }
