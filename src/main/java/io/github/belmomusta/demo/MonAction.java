6a0c99caec746a6fb3090d41fa7a799853cdad96
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2026-09-25 22:20:24 -0400

    demo

--- src/main/java/io/github/belmomusta/demo/MonAction.java
+++ src/main/java/io/github/belmomusta/demo/MonAction.java
@@ -4,9 +4,21 @@ package io.github.belmomusta.demo;
 import io.github.belmomusta.java.actions.sdk.Action;
 import io.github.belmomusta.java.actions.sdk.Core;
 
+import java.util.Map;
+
 public class MonAction implements Action {
     @Override
     public void run() throws Exception {
         Core.info("Hello World");
+        boolean flag = Core.getBooleanInput("flag", null);
+        Core.info("boolean flag : " + flag);
+        Core.setOutput("results", "1");
+        Core.setSecret("SECRET_007");
+        Core.info("my secret is SECRET_007");
+        Core.startGroup("Group 1");
+        Core.info("this line should be wrapped into a group");
+        Core.endGroup();
+
+        Core.warning("this is a warning", Map.of());
     }
 }
